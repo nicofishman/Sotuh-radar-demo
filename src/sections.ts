6cf82ca5ecd66ab67f7859630693004c9f4ec349
nicofishman <nicourifishman@gmail.com>
2022-10-23 12:24:36 -0300

    Sidebar with swappable Sections

---
+++ src/sections.ts
@@ -0,0 +1,10 @@
+
+import SectionA from './components/sections/SectionA.astro';
+import SectionB from './components/sections/SectionB.astro';
+import PokeSection from './components/sections/PokeSection.astro';
+
+export const Sections = {
+    A: SectionA,
+    Poke: PokeSection,
+    B: SectionB
+};
