3d430f69be4b692bab6a9806075379a10c3c116e
nicofishman <nicourifishman@gmail.com>
2022-10-28 13:01:33 -0300

    Recursirve Subsections

--- src/sections.ts
+++ src/sections.ts
@@ -1,11 +1,39 @@
 import SectionA from './components/sections/SectionA.astro';
 import SectionB from './components/sections/SectionB.astro';
+import SectionC from './components/sections/SectionC.astro';
+import SectionD from './components/sections/SectionD.astro';
 import PokeSection from './components/sections/PokeSection.astro';
 
-export const Sections: {
-    [key: string]: typeof SectionA;
-} = {
-    A: SectionA,
-    Poke: PokeSection,
-    B: SectionB
-};
+export type SectionsType = {
+    id: string,
+    children?: SectionsType[]
+    component?: typeof SectionA
+}
+
+export const Sections: SectionsType[] = [
+    {
+        id: 'A',
+        component: SectionA
+    },
+    {
+        id: 'D',
+        component: SectionD
+    },
+    {
+        id: 'B',
+        component: SectionB
+    },
+    {
+        id: 'SectionsTest',
+        children: [
+            {
+                id: 'C',
+                component: SectionC
+            },
+            {
+                id: 'Poke',
+                component: PokeSection
+            }
+        ]
+    }
+];
