2169fc58568a7ddf58f2c1556f93b512f96ecdb6
nicofishman <nicourifishman@gmail.com>
2022-10-30 18:44:42 -0300

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,tsx,astro}"],
+    content: ['./src/**/*.{html,tsx,astro}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'dark-green': '#03332b',
+                card: {
+                    DEFAULT: '#fc4c02',
+                    'light-orange': '#E95420',
+                    yellow: '#EFB73E'
+                }
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif']
+            },
+            backgroundImage: {
+                'card-gradient': 'linear-gradient(180deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0))'
+            }
+        }
     },
-    plugins: [],
-}
+    plugins: []
+};
